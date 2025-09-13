0d6255eb428b62c9b728a6ce4ae5a3a6bb8949ad
joudykarim <dodyjoudy70@gmail.com>
2025-09-13 08:31:51 -0700

    case3

---
+++ j.py
@@ -0,0 +1,18 @@
+import random
+print("welcome to this python application")
+print("what do you want to do for this app")
+print("1-add 2 numbers")
+print("2-print random number")
+print("3-print a random word")
+print("4-multiply 2 numbers")
+input=input("enter ur choice: ")
+
+match input:
+    case"1":
+        num1=int(input("enter first num= "))
+        num2=int(input("enter second num= "))
+        print(f"the sum of {num1} and {num2} is {num1+num2}")
+    case"2":
+        print("random number: "+random.randint(0,100)) 
+    case"3":
+        print("random word: "+random.choice(["apple","banana","cherry","date","fig"]))
